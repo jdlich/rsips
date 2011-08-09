bbd3835ed37a38c36d6fb864a2c9249421de6a65
Jacob Lichner <jacob.d.lichner@gmail.com>
2011-08-09 16:08:13 -0700

    refactored ext replace when changing format

--- lib/rsips/sips.rb
+++ lib/rsips/sips.rb
@@ -8,13 +8,13 @@ module Rsips::Sips
   end
   
   def format(type, options={})
-    options  = options[:compression] || "default"
-    file_ext = case type
+    options = options[:compression] || "default"
+    new_ext = case type
       when :jpeg then "jpg"
       when :tiff then "tif"
       else type
     end
-    new_image = "#{@img.split('.')[0..-2].join('.')}.#{file_ext}"
+    new_image = replace_ext(@img, new_ext)
     sips "-s format #{type} -s formatOptions #{options} #{@img} --out #{new_image}"
   end
   
@@ -22,5 +22,9 @@ module Rsips::Sips
   
   def sips(command)
     system "sips #{command}"
+  end
+  
+  def replace_ext(filename, new_ext)
+    filename.split('.')[0..-2].push(new_ext).join('.')
   end
 end
