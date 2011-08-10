d9a7bca5e5742dbb76a23809278a939f75068486
Jacob Lichner <jacob.d.lichner@gmail.com>
2011-08-10 08:54:01 -0700

    module before requires

--- lib/rsips.rb
+++ lib/rsips.rb
@@ -5,13 +5,13 @@ require 'fileutils'
 $:.unshift File.expand_path(File.dirname(__FILE__))
 
 # rsips
-require 'rsips/sips'
-require 'rsips/image'
-require 'rsips/collection'
-
 module Rsips
   
   # Current version of rsips
   VERSION = '0.0.1'
   
 end
+
+require 'rsips/sips'
+require 'rsips/image'
+require 'rsips/collection'
